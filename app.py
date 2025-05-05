719d31c1aeebbbf86480b328a13a04c5399058eb
Aslah Faseel K <144783423+aslahfaseel@users.noreply.github.com>
2025-05-05 12:47:25 +0530

    Update app new

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-   return "Hello World good to know!"
+   return "Hello World good to know new!"
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=8080)
